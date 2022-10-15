74ec3a2a7afd03730d2605e38e5a6f1328a9c2b6
yaoyiran <yaoyiran@umich.edu>
2022-10-15 22:27:10 +0100

    Update SampleCode.py

--- SampleCode.py
+++ SampleCode.py
@@ -4,8 +4,8 @@ import torch
 # Procrustes. Grabed from ContrastiveBLI repo: "https://github.com/cambridgeltl/ContrastiveBLI". Please refer to Appendix A.3 of paper "Improving Word Translation via Two-Stage Contrastive Learning".
 
 def procrustes(X_src, Y_tgt):
-    # X_src: source WEs of size (n, d1) 
-    # Y_tgt: target WEs of size (n, d2)
+    # X_src: source WEs of size (n, d1), np.array 
+    # Y_tgt: target WEs of size (n, d2), np.array
     # n: number of word translation pairs for training
     # d1 and d2: dimension of WEs. d1<=d2 (usually d1=d2), typically 300 for fastText WEs and 768 for mBERT WEs.
     X_dim = X_src.shape[1]
@@ -22,9 +22,9 @@ def procrustes(X_src, Y_tgt):
         V = np.concatenate((V,zeros_pad),axis=0)
         return np.dot(U, V)
 
-# Require: X_src of size (n, dim_X), Y_tgt of size (n, dim_Y)
-# Output: X_src_mapped
-linear_weights = procrustes(X_src, Y_tgt)
+# Require: X_src of size (n, dim_X), Y_tgt of size (n, dim_Y), torch.tensor
+# Output: X_src_mapped, torch.tensor
+linear_weights = procrustes(X_src.numpy(), Y_tgt.numpy())
 linear_weights = torch.tensor(linear_weights,dtype=torch.float32)
 
 linear_layer = torch.nn.Linear(dim_X,dim_Y,bias=False)
