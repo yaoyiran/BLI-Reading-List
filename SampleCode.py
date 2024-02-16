bfa40f69aef06a4d68ab1fbe6bbcf8ac5c73261e
yaoyiran <yaoyiran@umich.edu>
2024-02-16 01:16:27 +0000

    Update SampleCode.py

--- SampleCode.py
+++ SampleCode.py
@@ -1,7 +1,27 @@
+# Sample 1. Prompt Large Language Models (e.g., LLaMA) for BLI. Code grabbed from Prompt4BLI repo: "https://github.com/cambridgeltl/prompt4bli".
+# This is the current state-of-the-art BLI approach.
+
+from transformers import LlamaForCausalLM, LlamaTokenizer
+tokenizer = LlamaTokenizer.from_pretrained("huggyllama/llama-7b",padding_side='left')
+tokenizer.pad_token_id = tokenizer.eos_token_id
+model = LlamaForCausalLM.from_pretrained("huggyllama/llama-7b").cuda()
+model.eval()
+
+TXT = ["The word 'you' in Chinese is 你. The word 'he' in Chinese is"]  # In-context learning: a '.' token will follow the target word. 
+
+input_encs = tokenizer.batch_encode_plus(TXT,padding=True,add_special_tokens=True,return_tensors="pt")
+max_len = 5 + input_encs.input_ids.size(1)
+input_encs_cuda = {k:v.cuda() for k,v in input_encs.items()}
+outputs = model.generate(**input_encs_cuda,num_beams=5, num_return_sequences=5,max_length=max_len,output_scores=True,return_dict_in_generate=True,do_sample=False)
+output_sents = [tokenizer.decode(o, skip_special_tokens=True) for o in outputs.sequences]
+
+target_word = output_sents[0].split(TXT[0])[1].split()[0].split(".")[0]
+
+# Sample 2. Procrustes. Code grabbed from ContrastiveBLI repo: "https://github.com/cambridgeltl/ContrastiveBLI". Please refer to Appendix A.3 of paper "Improving Word Translation via Two-Stage Contrastive Learning".
+# A representative mapping-based BLI approach.
+
 import numpy as np
 import torch
-
-# Procrustes. Grabed from ContrastiveBLI repo: "https://github.com/cambridgeltl/ContrastiveBLI". Please refer to Appendix A.3 of paper "Improving Word Translation via Two-Stage Contrastive Learning".
 
 def procrustes(X_src, Y_tgt):
     # X_src: source WEs of size (n, d1), np.array 
@@ -32,3 +52,9 @@ linear_layer.weight.data = linear_weights.t()
 linear_layer.requires_grad_(False)
 
 X_src_mapped = linear_layer(X_src)
+
+
+
+
+
+
